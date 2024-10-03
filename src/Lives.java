373d3d8ade59338e6b0f3067085d4eb51c26dd01
sydney_sim_yf <sydney9191@gmail.com>
2024-10-03 12:30:58 +0800

    made the hearts into a nice image displaying health bar instead of just a string

--- src/Lives.java
+++ src/Lives.java
@@ -1,18 +1,43 @@
 package src;
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
 
 public class Lives {
     private int lives;
     private int screenHeight;
     private int screenWidth;
 
+    BufferedImage heartImage;
+    BufferedImage resizedImage;
+
+
 
     public Lives(int lives, int screenWidth, int screenHeight){
         this.lives=lives;
         this.screenHeight=screenHeight;
         this.screenWidth=screenWidth;
+
+        try{
+            heartImage= ImageIO.read(new File("data/heart.png"));
+            resizedImage=resizeImage(heartImage, 30, 30);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public BufferedImage resizeImage(BufferedImage image, int targetWidth , int targetHeight){
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.setComposite(java.awt.AlphaComposite.Src);
+        g2d.drawImage(image, 0, 0, targetWidth, targetHeight, null);
+        g2d.dispose(); 
+        return resizedImage;
     }
 
     public int getLives(){
@@ -25,7 +50,13 @@ public class Lives {
 
     public void draw(Graphics g){
         g.setColor(Color.CYAN);
-        g.drawString("Lives: " + lives, 10, 20);
+
+        for (int numHearts=0; numHearts<lives; numHearts++){
+            g.drawImage(resizedImage, 20+numHearts*40 , 20, null);
+        }
+        
     }
+
+
 
 }
